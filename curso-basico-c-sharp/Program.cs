30b2ae65a5cac9204daabb8219cdbd92e66b440d
Oscar Roman <oscarroman.pe@gmail.com>
2023-08-13 13:24:18 -0500

    setup and first lessons

---
+++ curso-basico-c-sharp/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var (valor1, valor2, valor3) = (10, 1, 2);
+
+Console.WriteLine("Los valores son: " + valor1 + valor2 + valor3);
+
+/*
+    == igual a
+    != diferente a
+    > mayor que
+    < menor que
+    >= mayor o igual
+    <= menor o igual
+*/
+
+bool resultado1 = valor1 == valor2;
+Console.WriteLine("El resultado de la comparacion valor1 == valor2: " + resultado1);
+
+bool resultado2 = valor1 != valor2;
+Console.WriteLine("El resultado de la comparacion valor1 != valor2: " + resultado2);
+
+bool resultado3 = valor1 > valor2;
+Console.WriteLine("El resultado de la comparacion valor1 > valor2: " + resultado3);
+
+bool resultado4 = valor1 < valor2;
+Console.WriteLine("El resultado de la comparacion valor1 < valor2: " + resultado4);
+
+bool resultado5 = valor1 >= valor2;
+Console.WriteLine("El resultado de la comparacion valor1 >= valor2: " + resultado5);
+
+bool resultado6 = valor1 <= valor2;
+Console.WriteLine("El resultado de la comparacion valor1 <= valor2: " + resultado6);
---
+++ strings/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Ricardo Pérez ID");
+int altura = 168;
+int edad = 89;
+string nombre = "Ricardo Pérez";
+string informacion = "Nació en Hidalgo, es estudiante de gastronomía y astronomía";
+var hobby = "deportista";
+
+string tarjetaDeIdentificacion = $"La información de {nombre}\n" +
+    $"es la siguiente, \nsu edad son {edad}, \nsu altura es {altura}. \n{informacion}" +
+    $"\ny como hobby es {hobby}";
+
+Console.WriteLine(tarjetaDeIdentificacion);
